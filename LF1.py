9b76cb17d185da49bc46124a12602701764b06ca
Jun <jake.jkchen@gmail.com>
2022-07-06 17:34:20 -0400

    modify LF1 to call Lex bot and send SNS message

--- LF1.py
+++ LF1.py
@@ -9,17 +9,27 @@ region = 'us-east-1'
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+lexbot = boto3.client('lex-runtime')
+sns = boto3.client('sns', region_name='us-east-1')
 
 host = 'https://search-posts-olyfhy4stfviygw2xf35znnvmi.us-east-1.es.amazonaws.com'
 index = 'posts'
 url = host + '/' + index + '/_search'
 
 def lambda_handler(event, context):
+    print(event)
+    lexresponse = lexbot.post_text(
+        botName = 'GetPost',
+        botAlias = 'chatGetPost',
+        userId = 'testuser',
+        inputText = event['queryStringParameters']['q']
+        )
+    print(lexresponse)
     query = {
         "size": 3,
         "query": {
             "multi_match": {
-                "query": event['queryStringParameters']['q'],
+                "query": lexresponse['slots']['topic'],
                 "fields": ["tags"]
             }
         }
@@ -39,8 +49,6 @@ def lambda_handler(event, context):
         "isBase64Encoded": False
     }
 
-    # Add the search results to the response
-    #response['body'] = r.text
     
     posts_list = json.loads(r.text)['hits']['hits']
     posts_id_list = [x['_id'] for x in posts_list]
@@ -58,4 +66,8 @@ def lambda_handler(event, context):
             posts_text.append(resp['Items'][0]['posts'])
     
     response['body'] = json.dumps(posts_text)
+    # SEND MESSAGES
+    sns.publish(TopicArn='arn:aws:sns:us-east-1:051792343076:chat_response_posts',
+            Message=response['body']
+                                    )
     return response
